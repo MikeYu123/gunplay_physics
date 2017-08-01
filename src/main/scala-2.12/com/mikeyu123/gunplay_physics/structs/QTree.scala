6e1357742aceae3c33c8fb311b76ab735a18bcbd
Alex <zaqepa@gmail.com>
2017-08-01 18:41:11 +0300

    + QTree insert

--- src/main/scala-2.12/com/mikeyu123/gunplay_physics/structs/QTree.scala
+++ src/main/scala-2.12/com/mikeyu123/gunplay_physics/structs/QTree.scala
@@ -9,45 +9,24 @@ class QTree(primitives: Set[GeometryPrimitive], aabb: AABB, capacity: Int) {
   //  val node_3: QTree =
   //  val node_4: QTree =
 
-  //  def sort_primitives: Map[AABB, Set[GeometryPrimitive]] = {
-  //  def sort_primitives: Any = {
-  //    //    primitives11
-  //    val s: Set[GeometryPrimitive] = Set(Rectangle(Point(0, 0), Point(8, 8), Point(0, 0), Point(8, 8)))
-  //    Set(s)
-  //
-  //    val base: Map[AABB, Set[GeometryPrimitive]] = Map[AABB, Set[GeometryPrimitive]]()
-  //    val sets =
-  //      primitives.fold(base) { case (sets1: Map[AABB, Set[GeometryPrimitive]], p: GeometryPrimitive) =>
-  //        val pAabb: AABB = p.getAabb
-  //        nodes_AABB.reduce { (sets2: Map[AABB, Set[GeometryPrimitive]], aabb: AABB) =>
-  //          val pr = aabb.intersects(pAabb)
-  //          if (pr) {
-  //            val v = sets2.get(aabb)
-  //            v match {
-  //              case Some(set) => {
-  //                sets2.updated(aabb, v.get + p)
-  //              }
-  //              case None => {
-  //                sets2.updated(aabb, Set(p))
-  //              }
-  //            }
-  //          }
-  //          sets2
-  //        }
-  //        sets1
-  //      }
-  //    sets
-  //  }
+//    def sort_primitives: Map[AABB, Set[GeometryPrimitive]] = {
+//      //    primitives11
+//      val s: Set[GeometryPrimitive] = Set(Rectangle(Point(0, 0), Point(8, 8), Point(0, 0), Point(8, 8)))
+//      Set(s)
+//
+//      val base: Map[AABB, Set[GeometryPrimitive]] = Map[AABB, Set[GeometryPrimitive]]()
+//      val sets: Map[AABB, Set[GeometryPrimitive]]  =
+//        primitives.fold(base){insert}
+//    }
 
   def insert(sets: Map[AABB, Set[GeometryPrimitive]], p: GeometryPrimitive): Map[AABB, Set[GeometryPrimitive]] = {
     val pAabb: AABB = p.getAabb
-    val w = nodesAabb.fold(sets) { case (set: Map[AABB, Set[GeometryPrimitive]], aabb: AABB) =>
+    nodesAabb.fold(sets) { case (set: Map[AABB, Set[GeometryPrimitive]], aabb: AABB) =>
       val pr = aabb.intersects(pAabb)
       if (pr) {
-        val v: Set[GeometryPrimitive] = sets.getOrElse(aabb, Set[GeometryPrimitive]()) + p
-        set.updated(aabb, v)
+        val newSet: Set[GeometryPrimitive] = sets.getOrElse(aabb, Set[GeometryPrimitive]()) + p
+        set.updated(aabb, newSet)
       } else set
-    }
-      Map[AABB, Set[GeometryPrimitive]]()
+    }.asInstanceOf[Map[AABB, Set[GeometryPrimitive]]]
   }
 }
--- src/test/scala-2.12/com/mikeyu123/gunplay_physics/structs/QTreeMethods.scala
+++ src/test/scala-2.12/com/mikeyu123/gunplay_physics/structs/QTreeMethods.scala
@@ -10,18 +10,17 @@ class QTreeMethods extends GraphicsSpec {
     Rectangle(Point(x - side / 2, y - side / 2), Point(x - side / 2, y + side / 2), Point(x + side / 2, y + side / 2), Point(x + side / 2, y - side / 2))
   }
 
-  it should "get min of 2 points" in {
-
+  it should "insert test" in {
     val s: Set[GeometryPrimitive] = Set(spawnRect(1.5, 6.5), spawnRect(2.5, 2.5), spawnRect(2.5, 5.5),
       spawnRect(4.5, 4.5), spawnRect(5.5, 5.5), spawnRect(6.5, 5.5), spawnRect(6.5, 7.5))
     val aabb: AABB = AABB(Point(1, 2), Point(7, 8))
     val capacity: Int = 3
     val qTree: QTree = new QTree(s, aabb, capacity)
-//    val q = qTree.sort_primitives
     val sets: Map[AABB, Set[GeometryPrimitive]] = Map()
-    qTree.insert(sets, spawnRect(1.5, 6.5))
-
-    //    Point(1d, 1d).min(Point(0d, 2d)) should equal(Point(0d, 1d))
+    val q: Map[AABB, Set[GeometryPrimitive]] = qTree.insert(sets, spawnRect(1.5, 6.5))
+    q should equal {
+          Map((AABB(Point(1,5), Point(4,8)), Set(spawnRect(1.5, 6.5))))
+    }
   }
 
 }
