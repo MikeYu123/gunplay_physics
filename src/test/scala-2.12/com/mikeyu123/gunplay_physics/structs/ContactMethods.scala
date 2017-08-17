f3f98be6b67aa290ba69eb16c7619b8117769706
Alex <zaqepa@gmail.com>
2017-08-17 17:53:45 +0300

    + Tests for hashcode $ equals

--- src/test/scala-2.12/com/mikeyu123/gunplay_physics/structs/ContactMethods.scala
+++ src/test/scala-2.12/com/mikeyu123/gunplay_physics/structs/ContactMethods.scala
@@ -14,24 +14,44 @@ class ContactMethods extends GraphicsSpec {
   val c2 = Contact(a, a)
   val s: Set[Contact] = Set[Contact](c0, c1, c2)
 
-  it should "contact hashCode test" in {
-    c0.hashCode() should equal {
-      c1.hashCode()
-    }
-  }
-
-  it should "contact equals test" in {
-    c0.equals(c1) should equal {
+  it should "contact hashCode test 0" in {
+    c0.hashCode() == c1.hashCode() should equal {
       true
     }
   }
-  
+
+  it should "contact hashCode test 1" in {
+    c0.hashCode() == c2.hashCode() should equal {
+      false
+    }
+  }
+
+  it should "contact equals test 0" in {
+    c0.equals(c1) should equal {
+      true
+    }
+    c1.equals(c0) should equal {
+      true
+    }
+  }
+
+  it should "contact equals test 1" in {
+    c0.equals(c2) should equal {
+      false
+    }
+    c2.equals(c0) should equal {
+      false
+    }
+  }
+
   it should "contact hashset test" in {
     val hs: HashSet[Contact] = HashSet()
     val res = s.foldLeft(hs)(_ + _)
     res.size should equal {
       2
     }
+    res should equal {
+      HashSet(c0, c2)
+    }
   }
-
 }
